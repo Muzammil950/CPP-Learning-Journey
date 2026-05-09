a1342b4e5a79c130c760c8f4bf0a2088aab4c8dc
Muzammil950 <muzammil9709@gmail.com>
2026-05-09 17:26:34 +0530

    add new files

---
+++ maths/ques03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std ;
+int main() {
+   int  x ;
+   x = 4 ;
+   int  y ;
+   y = 5 ;
+
+   cout << x + y ;
+
+}
+
+#include <iostream>
+using namespace std ;
+int main () {
+    int x = 4;
+    int y = 5;
+   
+    cout << x + y;
+}
+
+#include <iostream>
+using namespace std ;
+int main () {
+    int x , y ;
+    x = 4, y = 5;
+    cout << x + y ;
+}
+
+#include <iostream>
+using namespace std ;
+int main () {
+    int x = 4, y = 5 ;
+    cout << x + y;
+}
