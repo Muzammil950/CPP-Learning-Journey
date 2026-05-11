6383a2f5d35b827d76684d076054596ae0a82cad
Muzammil950 <muzammil9709@gmail.com>
2026-05-11 20:03:50 +0530

    add new files

---
+++ operaters/arthematic.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+// using namespace std ;
+// int main () {
+//     int x = 5, y = 2 ;
+//         cout << x + y << endl ;
+//         cout << x - y << endl ;
+//         cout << x * y << endl ;
+//         cout << x / y ;
+// }
+
+#include <iostream>
+using namespace std ;
+int main () {
+    float  x = 5, y = 2 ;
+        cout << x + y << endl ;
+        cout << x - y << endl ;
+        cout << x * y << endl ;
+        cout << x / y ;
+}
