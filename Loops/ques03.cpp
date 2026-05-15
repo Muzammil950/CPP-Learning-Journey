0efa5b91bc78156a201cdcf17176d42ac3e256d6
Muzammil950 <muzammil9709@gmail.com>
2026-05-15 20:19:10 +0530

    add new programs

---
+++ Loops/ques03.cpp
@@ -0,0 +1,17 @@
+// print all the even num from 1 to 100
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     for(int i=1; i<=100; i++){
+//         if(i%2==0) cout<<i<<" ";
+//     }
+// }
+
+#include<iostream>
+using namespace std;
+int main () {
+    for(int i=2; i<=100; i+=2){
+        cout<<i<<" ";
+    }
+}
---
+++ Loops/ques04.cpp
@@ -0,0 +1,9 @@
+// print all the odd num in 1 to 100
+
+#include <iostream>
+using namespace std;
+int main () {
+    for(int i=1; i<=100; i++){
+        if(i%2!=0) cout<<i<<" ";
+    }
+}
